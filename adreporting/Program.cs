9f9f6508c505d4fc25e9f4a263aae84480f637ab
John Zetterman <jzetterman@outlook.com>
2024-04-27 15:01:01 -0400

    Initial commit with basic LDAP search

---
+++ adreporting/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System.ComponentModel.DataAnnotations;
+using System.Reflection.PortableExecutable;
+using System.DirectoryServices.AccountManagement;
+
+DirectoryEntry directoryEntry = new DirectoryEntry("LDAP://ua.ad.alaska.edu");
+DirectorySearcher searcher = new DirectorySearcher(directoryEntry) {
+    PageSize = int.MaxValue,
+    FilterUIHintAttribute = "(&(objectCategory=person))"
+}
+
+searcher.PropertiesToLoad.Add("sn");
+
+var result = searcher.FindOne();
+
+if (result == null) {
+    return;
+}
+
+string surname;
+
+if (result.Properties.Contains("sn")) {
+    surname = result.Properties["sn"][0].ToString();
+    Console.WriteLine(surname);
+} else {
+    Console.WriteLine("No user found!");
+}
